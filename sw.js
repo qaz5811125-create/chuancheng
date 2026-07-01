a3647c0d910402221409be94fe806b83392bba3b
qaz5811125-create <qaz5811125@gmail.com>
2026-07-02 04:51:53 +0800

    Fix monster status display v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chuan-cheng-beta-0-5-2-monster-formation";
+const CACHE_NAME = "chuan-cheng-beta-0-5-2-status-v3";
 const ASSETS = [
   "./",
   "index.html",
@@ -33,13 +33,12 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("index.html")))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match("index.html")))
   );
 });
