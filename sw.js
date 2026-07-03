7fc9c436f2ae447996d071e5307564abeafe14b1
qaz5811125-create <qaz5811125@gmail.com>
2026-07-03 16:59:27 +0800

    Beta 0.9.4 independent map stages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chuancheng-cache-v0.9.3-number-gunner-hotfix-v2";
+const CACHE_NAME = "chuancheng-cache-v0.9.4-independent-map-stages-v1";
 const ASSETS = [
   "./",
   "index.html",
